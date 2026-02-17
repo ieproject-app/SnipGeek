3e1eb6d0b0d3c7b6519c107a976012b25431d228
Iwan Efendi <iwan.efndi@gmail.com>
2026-02-17 02:52:37 +0000

    berarti saya tulis ![...]() gini aja maka kamu akan otomatis pindahkan d

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -17,7 +17,7 @@ export function Footer({ dictionary, translationsMap }: { dictionary: Dictionary
       { id: 'footer-contact', title: dictionary.navigation.contact, href: '/contact' },
     ];
 
-    const authorName = "Iwan Efendi";
+    const authorName = "SnipGeek";
     const authorAvatar = "/images/profile/profile.png";
 
     return (
@@ -73,7 +73,7 @@ export function Footer({ dictionary, translationsMap }: { dictionary: Dictionary
 
             {/* Section 3: Standard Footer */}
             <div className="mt-16 sm:mt-24 py-8 border-t">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4 text-sm text-muted-foreground">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row justify-center items-center gap-4 text-sm text-muted-foreground">
                     <p>&copy; {new Date().getFullYear()} {authorName}. All Rights Reserved.</p>
                     <div className="flex items-center gap-4">
                         <LanguageSwitcher translationsMap={translationsMap} />
