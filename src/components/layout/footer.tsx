149f0978ebb3d71cee41e451bcb200954f4e4895
Iwan Efendi <iwan.efndi@gmail.com>
2026-02-17 02:57:39 +0000

    footer bagian ini letak dibagian paling bawah aja, jadi en/id dan switch

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -74,11 +74,11 @@ export function Footer({ dictionary, translationsMap }: { dictionary: Dictionary
             {/* Section 3: Standard Footer */}
             <div className="mt-16 sm:mt-24 py-8 border-t">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col justify-center items-center gap-4 text-sm text-muted-foreground">
-                    <p>&copy; {new Date().getFullYear()} {authorName}. All Rights Reserved.</p>
                     <div className="flex items-center gap-4">
                         <LanguageSwitcher translationsMap={translationsMap} />
                         <ThemeSwitcher />
                     </div>
+                    <p>&copy; {new Date().getFullYear()} {authorName}. All Rights Reserved.</p>
                 </div>
             </div>
         </footer>
