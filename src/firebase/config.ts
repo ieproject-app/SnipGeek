5080796b3415b7e809b0a6bbc46cba5dbf25a352
Iwan Efendi <iwan.efndi@gmail.com>
2026-02-28 13:57:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8697076532-14512",
+  "appId": "1:910122792787:web:183c7da1ae114ac74ad3a6",
+  "apiKey": "AIzaSyAtcWDqJ28CI3Pc4pHDN5ET7iTsgmxe2-E",
+  "authDomain": "studio-8697076532-14512.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "910122792787"
+};
